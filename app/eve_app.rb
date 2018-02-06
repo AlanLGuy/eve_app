57b9c2179d87d961cf69f3d64aa2262af823d0c0
Alan.L.Guy@gmail.com <uz9Mah0B>
2018-02-06 17:47:42 -0500

    Revert "Revert "reconfiguring local server""
    This reverts commit e634441

--- app/eve_app.rb
+++ app/eve_app.rb
@@ -7,21 +7,26 @@ require 'haml'
 require 'yaml'
 require_relative 'lib/character'
 
-
-if File.exist?(File.expand_path('../config/private.yaml', __FILE__))
-  private_data = YAML.load_file('app/config/private.yaml')
-  CLIENT_ID = private_data[:CLIENT_ID]
-  CLIENT_SECRET = private_data[:CLIENT_SECRET]
-else
-  CLIENT_ID = ENV['CLIENT_ID']
-  CLIENT_SECRET = ENV['CLIENT_SECRET']
-end
-
-REDIRECT_URI = 'https://thawing-hollows-77046.herokuapp.com/authorized'
-SCOPE = 'esi-skills.read_skills.v1 esi-skills.read_skillqueue.v1 esi-wallet.read_character_wallet.v1 esi-assets.read_assets.v1 esi-planets.manage_planets.v1 esi-markets.structure_markets.v1 esi-characters.read_standings.v1 esi-characters.read_agents_research.v1 esi-industry.read_character_jobs.v1 esi-markets.read_character_orders.v1 esi-characters.read_blueprints.v1 esi-contracts.read_character_contracts.v1 esi-wallet.read_corporation_wallets.v1 esi-industry.read_corporation_jobs.v1 esi-industry.read_character_mining.v1'
-STATE = 'thatsanicestateyouhavethere'
-
 class EveApp < Sinatra::Base
+
+  if settings.environment == :local
+    set :port, 7272
+
+    private_data = YAML.load_file('app/config/private.yaml')
+    REDIRECT_URI = 'http://localhost:7272/authorized'
+    CLIENT_ID = private_data[:CLIENT_ID]
+    CLIENT_SECRET = private_data[:CLIENT_SECRET]
+  elsif settings.environment == :development
+    REDIRECT_URI = 'https://thawing-hollows-77046.herokuapp.com/authorized'
+    CLIENT_ID = ENV['CLIENT_ID']
+    CLIENT_SECRET = ENV['CLIENT_SECRET']
+  else
+    raise("Error: Environment#{settings.environment} is not configured")
+  end
+
+  SCOPE = 'esi-skills.read_skills.v1 esi-skills.read_skillqueue.v1 esi-wallet.read_character_wallet.v1 esi-assets.read_assets.v1 esi-planets.manage_planets.v1 esi-markets.structure_markets.v1 esi-characters.read_standings.v1 esi-characters.read_agents_research.v1 esi-industry.read_character_jobs.v1 esi-markets.read_character_orders.v1 esi-characters.read_blueprints.v1 esi-contracts.read_character_contracts.v1 esi-wallet.read_corporation_wallets.v1 esi-industry.read_corporation_jobs.v1 esi-industry.read_character_mining.v1'
+  STATE = 'thatsanicestateyouhavethere'
+
   set :haml, :format => :html5
 
 
@@ -41,9 +46,8 @@ class EveApp < Sinatra::Base
     redirect('index')
   end
 
-  get '/'do
+  get '/' do
     parameters = "?response_type=code&redirect_uri=#{REDIRECT_URI}&client_id=#{CLIENT_ID}&scope=#{SCOPE}&state=#{STATE}"
-    puts parameters
 
     if session[:access_token]
       redirect('index')
@@ -60,4 +64,3 @@ class EveApp < Sinatra::Base
   end
 
 end
-# Launchy.open()
